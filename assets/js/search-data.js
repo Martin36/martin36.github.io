cb4f3ad295ad5073ebbec8c7dccb1818ca140d24
Martin36 <martin.funkquist@gmail.com>
2025-06-29 19:59:24 +0000

    Deploying to gh-pages from @ Martin36/martin36.github.io@ac26305de70392d4655908c9784ae568888b04c9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here is an overview of my professional experience, education, and skills. For more details, download the PDF version by clicking on the icon to the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
